2b5134528470909bdc661f722b68f3dea639ea9d
Alex <alexbelkacoder@gmail.com>
2022-12-11 21:02:00 +0300

    написание классов

--- index.js
+++ index.js
@@ -1,145 +1,131 @@
+let btn = document.getElementById("pressBtn");
+let root = document.getElementById("root");
+let finalPrice = [];
 
-let btn = document.getElementById('pressBtn');
-let root = document.getElementById('root');
-let menu = document.getElementById('menu');
-
- async function createRequest(){
-  
- return fetch('https://jsonplaceholder.typicode.com/users').then(response =>{
-
-    return response.json()
-
-  })
-
+async function createRequest() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => {
+      return response.json();
+    }
+  );
 }
 
-class PersonsList{
+class PersonsList {
+  constructor() {}
 
-    constructor(){  }
+  async fetchPosts() {
+    let responseUsers = await createRequest();
 
-    async fetchPosts(){
- 
-        const responseUsers = await createRequest() 
-        
-        for(let i = 0; i < responseUsers.length; i++){
-        
-            Object.keys(responseUsers[i]).forEach((item) => {
-        
-               if(item !== 'id' && item !== 'name' && item !=='username' && item !=='email'){
-        
-                  delete responseUsers[i][item]
-        
-               }
-            })
-          }   
-       
-    for(let i in responseUsers){
-
-      const person = new PersonItem(responseUsers[i])
-
-      person.render()
-
+    for (let i = 0; i < responseUsers.length; i++) {
+      Object.keys(responseUsers[i]).forEach((item) => {
+        if (
+          item !== "id" &&
+          item !== "name" &&
+          item !== "username" &&
+          item !== "email"
+        ) {
+          delete responseUsers[i][item];
+        }
+      });
     }
 
-     }
+    const personsList = document.createElement("ul");
+    personsList.className = "product-list";
 
-   async getData(){
+    for (let i in responseUsers) {
+      const person = new PersonItem(responseUsers[i]);
 
-    const data = await fetchPosts()
-   
-    console.log(data)
+      const personItem = person.render();
 
-   }
+      personsList.append(personItem);
+    }
 
-     begin(){
-       
-        const btn = document.getElementById('pressBtn');
+    const renderHook = document.getElementById("root");
 
-        btn.addEventListener('click',this.fetchPosts.bind(this))
+    renderHook.append(personsList);
+  }
 
-     }
+  begin() {
+    const btn = document.getElementById("pressBtn");
+    btn.addEventListener("click", this.fetchPosts.bind(this));
+  }
 }
 
+class PersonItem {
+  constructor(person) {
+    this.person = person;
+  }
 
-let getData = new PersonsList().begin()
+  identifyItem() {
+    PersonBasket.addItem(this.person);
+  }
 
-
-class PersonItem{
-    constructor(person){
-      this.person = person
-    }
-
-    render(){
- const prodItem = document.createElement('li')
- prodItem.className = 'prod-item'
- prodItem.innerHTML = `
+  render() {
+    const prodItem = document.createElement("li");
+    prodItem.className = "prod-item";
+    prodItem.innerHTML = `
  <h2>${this.person.id}</h2>
  <h3>${this.person.name}</h3>  
  <h4>${this.person.email}</h4>
  <h5>${this.person.username}</h5>
- <button>Press</button>
- ` 
-const menu = document.getElementById('menu')
-
- menu.insertAdjacentElement('afterbegin',prodItem)
-
+ <button>Купить раба</button>
+ `;
+    const getButton = prodItem.querySelector("button");
+    getButton.addEventListener("click", this.identifyItem.bind(this));
+    return prodItem;
   }
 }
 
+let result = new PersonsList().begin();
 
-// for(let i = 0; i < collection.length; i++){
+class PersonBasket {
+  constructor() {}
 
-// let target = new PersonsItem(collection[i]).person
+  set totalResult(value) {
+    console.log("!!");
+    finalPrice = value;
+    this.totalOutput.innerHTML = `<h3>Total: \$${this.totalSum}</h3>`;
+  }
 
-// console.log(target)
+  get totalSum() {
+    const sum = finalPrice.reduce(
+      (prevValue, curValue) => prevValue + curValue,
+      0
+    );
+    return sum;
+  }
 
-// }
+  static addItem(product) {
+    console.log(product);
+    console.log(this.totalOutput);
+    // const updatedItems = [...finalPrice];
+    // finalPrice.push(product);
+    // this.totalResult = updatedItems;
+  }
 
-// var html = collection.map(function(item) {
-//     return '<li>' + item.id + ' ' + item.name + ' (' + item.email + ')</li>'
-//   })
-//   menu.insertAdjacentHTML('afterbegin', html.join(' '))
+  render() {
+    this.item = new PersonItem();
+    const section = document.createElement("section");
+    section.innerHTML = `
+    <h3>Total:\$${0}</h3>
+    <h3>Amount:</h3>
+    <button>Order Now!!</button>
+    `;
+    section.className = "cart";
+    this.totalOutput = section.querySelector("h3");
 
-// class PersonItem{
-// constructor(person){
-// this.person = person
-// }
-// create(){
-//     // const person = document.createElement('li');
-//     // person.className = 'person';
-//     // person.innerHTML = `<div>
-//     // <h3>${this.person.id}</h3>
-//     // <h3>${this.person.name}</h3>
-//     // <h3>${this.person.email}</h3>
-//     // </div>`;
-//     console.log('!!!')
-//  }
-// }
+    return section;
+  }
+}
 
+let basket = new PersonBasket().render();
 
-// class PersonsItem{
-// constructor(item,name,id,email){
-// this.name = item.name
-// this.id = item.id
-// this.email = item.email
-//     }
-//     create(){
-//         console.log('!!!')
-//     }
-// }
+class SlaveShop {
+  constructor() {}
+  static render() {
+    const renderHook = document.getElementById("root");
+    renderHook.append(basket);
+  }
+}
 
-// let r = new PersonsItem();
-
-// console.log(r)
-
-// btn.addEventListener('click',fetchPosts)
-
-
-
-
-
-
-
-
-
-
+SlaveShop.render();
